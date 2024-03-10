35599af22f7f6153f9ff11d3c2316b03e94d95eb
Dima Berry <dimaberry1@gmail.com>
2024-03-10 15:39:18 +0100

    feat: solve returns a word from letters whose positions are provided as an object

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -140,8 +140,16 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const obj = {};
+  const entri = Object.entries(lettersObject);
+  entri.forEach((e) => {
+    const letter = e[0];
+    e[1].forEach((num) => {
+      obj[num] = letter;
+    });
+  });
+  return Object.values(obj).join('');
 }
 
 /**
